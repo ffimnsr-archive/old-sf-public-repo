271d75e99100852b36259a8aaa82a2e30afb61c4
ffimnsr <ffimnsr@outlook.com>
2018-05-31 18:53:34 +0800

    > initial commit

---
+++ src/widgets/HelloWorld.ts
@@ -0,0 +1,17 @@
+import WidgetBase from '@dojo/widget-core/WidgetBase';
+import { v } from '@dojo/widget-core/d';
+
+import * as css from './styles/helloWorld.m.css';
+
+const logo = require('./../img/logo.svg');
+
+export class HelloWorld extends WidgetBase {
+	protected render() {
+		return v('div', { classes: css.root }, [
+			v('img', { src: logo, classes: css.logo }),
+			v('div', { classes: css.label }, ['Hello, Dojo 2 World!'])
+		]);
+	}
+}
+
+export default HelloWorld;
